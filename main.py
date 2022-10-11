9792a1647656ea68782d9ace6604500ce4da6d58
Your Name <you@example.com>
2022-10-11 11:50:54 -0700

    realrx

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 for i in range (len(num2)):
     if num2[i] not in num1:
+        print("False")
         break
-if i==len(num2):
-    print("True")
 else:
-    print("False")
+    print("True")
 # ******************************
 # Make your Code
 # ******************************
